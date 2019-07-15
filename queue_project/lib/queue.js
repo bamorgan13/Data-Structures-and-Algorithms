9363c2859b7e57191369401d93ef9f6ea5e7e1f0
Bryce Morgan <bamorgan13@gmail.com>
2019-07-15 19:21:50 -0400

    Implement queue ADT

--- queue_project/lib/queue.js
+++ queue_project/lib/queue.js
@@ -12,7 +12,7 @@
 // Constraints:
 // ------------
 //
-// Make sure the time and space complexity of each is equivalent to those 
+// Make sure the time and space complexity of each is equivalent to those
 // in the table provided in the Time and Space Complexity Analysis section
 // of your Stack reading!
 //
@@ -21,11 +21,47 @@
 // -----------
 
 class Node {
-
+	constructor(val) {
+		this.value = val;
+		this.next = null;
+	}
 }
 
 class Queue {
+	constructor() {
+		this.front = null;
+		this.back = null;
+		this.length = 0;
+	}
 
+	enqueue(val) {
+		const newNode = new Node(val);
+		if (!this.length) {
+			this.front = newNode;
+			this.back = newNode;
+		}
+		this.back.next = newNode;
+		this.back = newNode;
+		this.length++;
+		return this.length;
+	}
+
+	dequeue() {
+		if (!this.length) return null;
+
+		const removed = this.front;
+		this.front = this.front.next;
+		this.length--;
+		if (!this.length) {
+			this.front = null;
+			this.back = null;
+		}
+		return removed.value;
+	}
+
+	size() {
+		return this.length;
+	}
 }
 
 exports.Node = Node;
