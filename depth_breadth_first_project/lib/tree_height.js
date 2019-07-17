a6b5b26221e604598fd6a4980ea4b6b2c16e77d4
Bryce Morgan <bamorgan13@gmail.com>
2019-07-16 21:10:12 -0400

    Implement tree sum and tree height

--- depth_breadth_first_project/lib/tree_height.js
+++ depth_breadth_first_project/lib/tree_height.js
@@ -1,8 +1,13 @@
-function treeHeight(root) {
+function treeHeight(root, height = -1) {
+	if (!root) return height;
+	height++;
+	const left = treeHeight(root.left, height);
+	const right = treeHeight(root.right, height);
+	const maxHeight = Math.max(left, right);
 
+	return maxHeight;
 }
 
-
 module.exports = {
-    treeHeight
+	treeHeight
 };
